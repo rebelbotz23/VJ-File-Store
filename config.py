3e9652ce9d6f905558604d19a6994b0da3e63222
rebel <rebelbotzzz04@gmail.com>
2024-12-15 15:16:20 +0530

    Update config.py

--- config.py
+++ config.py
@@ -18,8 +18,8 @@ def is_enabled(value, default):
         return default
       
 # Bot Information
-API_ID = int(environ.get("API_ID", ""))
-API_HASH = environ.get("API_HASH", "")
+API_ID = int(environ.get("API_ID", "26162072"))
+API_HASH = environ.get("API_HASH", "ba25181c01b50d945748801b6c8b6ecc")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
 
 PICS = (environ.get('PICS', 'https://graph.org/file/ce1723991756e48c35aa1.jpg')).split() # Bot Start Picture
