5a910057f09e7f3b764d7c35ae6c982cb698e489
rebel <rebelbotzzz04@gmail.com>
2024-12-15 15:17:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -20,7 +20,7 @@ def is_enabled(value, default):
 # Bot Information
 API_ID = int(environ.get("API_ID", "26162072"))
 API_HASH = environ.get("API_HASH", "ba25181c01b50d945748801b6c8b6ecc")
-BOT_TOKEN = environ.get("BOT_TOKEN", "")
+BOT_TOKEN = environ.get("BOT_TOKEN", "7638260756:AAHe-cAXJEUkYxJBOKg4Uth38oKafROkX0g")
 
 PICS = (environ.get('PICS', 'https://graph.org/file/ce1723991756e48c35aa1.jpg')).split() # Bot Start Picture
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '').split()]
